915fd3f9392c9316763463c0ece309c2bbd19dd8
Ashley <19aar94@gmail.com>
2024-01-22 11:09:42 -0600

    Share Button

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Share from './Share'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Share description = "I hope this works!"/>
+     <a href="https://github.com/Liftoff-2024-Coding-Angels/liftoff-group-anna-2/tree/main" className="App-link" target="_blank">Github Repo</a>
+    </>
+  )
+  }
+
+export default App

289b73be177af3c01bfc0cee451632765bfa3ea3
haylied <hayliedarland@gmail.com>
2024-01-23 17:16:24 -0600

    "fixing push on PC"

--- launchcodecapstone/LaunchCodeCapstone/Controllers/HaveWatchedController.cs
+++ launchcodecapstone/LaunchCodeCapstone/Controllers/HaveWatchedController.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 namespace LaunchCodeCapstone.Controllers
 {
-	public class HasWatchedController
+	public class HasWatchedController : Controller
 	{
-		public HasWatchedController()
+		private MovieDbContext context;
+		public HasWatchedController(MovieDbContext dbContext)
 		{
+			context = dbContext;
+		}
+
+		public IActionResult Index()
+		{
+			List<HaveWatched> haveOrHaveNotWatched = context.Watchd.ToList();
+			return View(haveOrHaveNotWatched);
+		}
+
+		public IActionResult AddHaveOrHAveNOtWatched() 
+		{
+		
 		}
 	}
 }
--- launchcodecapstone/LaunchCodeCapstone/Models/HaveWatched.cs
+++ launchcodecapstone/LaunchCodeCapstone/Models/HaveWatched.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
 namespace LaunchCodeCapstone.Models
 {
 	public class HasWatched
 	{
-		public HasWatched()
+        public int HaveWatchedId { get; set; }	]
+		public bool HaveOrHaveNotWatched { get; set; }
+
+		public HasWatched(bool haveOrHaveNotWatched)
 		{
+			HaveOrHaveNotWatched = haveOrHaveNotWatched;
 		}
-	}
+
+        public HasWatched()
+        {
+        }
+    }
 }
 

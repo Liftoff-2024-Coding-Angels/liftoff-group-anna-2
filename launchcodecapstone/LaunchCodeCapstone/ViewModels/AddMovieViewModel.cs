3afe8a634a5912c407d6d11ca4f3fb25bb3eda9f
haylied <hayliedarland@gmail.com>
2024-01-23 21:32:15 -0600

    AddMovieViewModel Update

--- launchcodecapstone/LaunchCodeCapstone/ViewModels/AddMovieViewModel.cs
+++ launchcodecapstone/LaunchCodeCapstone/ViewModels/AddMovieViewModel.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using LaunchCodeCapstone.Models;
+
 namespace LaunchCodeCapstone.ViewModels
 {
 	public class AddMovieViewModel
 	{
-		public AddMovieViewModel()
+		[Required(ErrorMessage = "A movie title is required.")]
+        public string Title { get; set; }
+
+		[Required(ErrorMessage = "A date is required.")]
+		public DateTime Date { get; set; }
+
+		[Required(ErrorMessage = "A selection is required.")]
+		public HaveWatched HaveWatched { get; set; }
+
+        public AddMovieViewModel()
 		{
 		}
 	}

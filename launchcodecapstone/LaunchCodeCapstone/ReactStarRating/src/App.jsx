1c89f90de11635ba933762849d5f4cea08831336
haylied <hayliedarland@gmail.com>
2024-01-23 21:08:13 -0600

    5 star rating icons

---
+++ launchcodecapstone/LaunchCodeCapstone/ReactStarRating/src/App.jsx
@@ -0,0 +1,13 @@
+import AddStarRating from "./AddStarRating";
+("./AddStarRating.jsx");
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AddStarRating />
+    </div>
+  );
+}
+
+export default App;

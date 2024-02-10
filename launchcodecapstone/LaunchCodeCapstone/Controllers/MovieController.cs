58fb6b3720236e05d1e8421443a9efba50a19c6c
Ashley <19aar94@gmail.com>
2024-02-09 22:54:48 -0600

    SearchBar

---
+++ launchcodecapstone/LaunchCodeCapstone/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using LaunchCodeCapstone.Data;
+using LaunchCodeCapstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaunchCodeCapstone.Controllers
+{
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+
+        public MovieController(MovieDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SearchBar()
+
+        {
+            List<Movie> movies = context.Movies.ToList();
+
+            return View(movies);
+        }
+    }
+}
--- launchcodecapstone/LaunchCodeCapstone/Data/MovieDbContext.cs
+++ launchcodecapstone/LaunchCodeCapstone/Data/MovieDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LaunchCodeCapstone.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LaunchCodeCapstone.Data
@@ -6,13 +7,16 @@ namespace LaunchCodeCapstone.Data
     public class MovieDbContext : IdentityDbContext
     {
 
-        //public DbSet<Movie>? Movies { get; set; }
+       public DbSet<Movie>? Movies { get; set; }
         //need to connect to Hailey's to see all classes/tables
         public MovieDbContext(DbContextOptions<MovieDbContext> options)
             : base(options)
         {
         }
        
+        public MovieDbContext() 
+        {
+        }
 
     }
 }

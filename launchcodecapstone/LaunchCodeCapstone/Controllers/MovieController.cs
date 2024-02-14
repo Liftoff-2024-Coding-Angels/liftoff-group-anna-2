b25963b165a871b08c3e4a14b5880b1a79295326
Ashley <19aar94@gmail.com>
2024-02-13 20:51:06 -0600

    APIworkswithView?

--- launchcodecapstone/LaunchCodeCapstone/Controllers/MovieController.cs
+++ launchcodecapstone/LaunchCodeCapstone/Controllers/MovieController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LaunchCodeCapstone.Controllers
 {
-    public class MovieController1 : Controller
+    public class MovieController : Controller
     {
         public IActionResult Index()
         {
@@ -14,14 +14,15 @@ namespace LaunchCodeCapstone.Controllers
 
         [HttpGet]
 
-        public async Task<IActionResult> Results()
+        public async Task<IActionResult> Results(string searchTerm)
         {
             var movieApi = MovieDbFactory.Create<IApiMovieRequest>().Value;
 
-            ApiSearchResponse<MovieInfo> response = await movieApi.SearchByTitleAsync("Cinderella");
+            ApiSearchResponse<MovieInfo> response = await movieApi.SearchByTitleAsync(searchTerm);
 
             foreach (MovieInfo info in response.Results)
             {
+
                 if (ModelState.IsValid)
                 {
                     Models.Movie movie = new Models.Movie()
@@ -29,31 +30,21 @@ namespace LaunchCodeCapstone.Controllers
                         Title = info.Title,
                         Overview = info.Overview
                     };
-                   /* context.Movies.Add(movie);
-                    context.SaveChanges();
-                   */
+                    /* context.Movies.Add(movie);
+                     context.SaveChanges();
+                    */
                     return View(Results);
+
+
+
                 }
             }
-            return View("Search");
+            return View("Index");
         }
-        /*
-       
-        /*public async Task<IActionResult> Results(string search)
-        {
-            var movieApi = MovieDbFactory.Create<IApiMovieRequest>().Value;
-
-            ApiSearchResponse<MovieInfo> response = await movieApi.SearchByTitleAsync(search);
-
-            foreach (MovieInfo info in response.Results)
-            {
-                return View("APIdisplay", info);
-            }
-            return View("Search");
-        */
     }
-
 }
+    
+    
 /*public IActionResult AddMovie(response)
       {
           if (ModelState.IsValid)
--- launchcodecapstone/LaunchCodeCapstone/Models/Movie.cs
+++ launchcodecapstone/LaunchCodeCapstone/Models/Movie.cs
@@ -1,14 +1,27 @@
-﻿namespace LaunchCodeCapstone.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LaunchCodeCapstone.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string Overview { get; set; }
 
-        public User User { get; set; }
-        public string Overview { get; internal set; }
+        public User? User { get; set; }
 
-        public Rating Ratings { get; set; }
+        public Rating? Ratings { get; set; }
+
+       public Movie(string title, string overview ) 
+        {
+            Title = title;
+            Overview = overview;
+        }
+
+        public Movie()
+        {
+        }
     }
+
 }

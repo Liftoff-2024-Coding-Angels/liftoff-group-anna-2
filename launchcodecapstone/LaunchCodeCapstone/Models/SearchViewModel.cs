61f9ccfe16c1081cbc8d4e18f37235ac76d8a8e4
Ashley <19aar94@gmail.com>
2024-02-18 14:20:41 -0600

    SearchBarconnectstoAPI

---
+++ launchcodecapstone/LaunchCodeCapstone/Models/SearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LaunchCodeCapstone.Models
+{
+    public class SearchViewModel
+    {
+        
+           public string? UserSearch {  get; set; } 
+        
+
+    }
+}

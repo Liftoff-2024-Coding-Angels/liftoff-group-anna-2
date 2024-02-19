47c86985fcbb0764765e35e8a47abad7df69c246
xiomaramendoza <mendozacolonx@gmail.com>
2024-02-19 07:28:15 -0600

    nav bar changes based on sign in status. show users is restricted to admin role. admin seed is unable to log in. users table displays and delete is functional.

--- LaunchCodeCapstone/LaunchCodeCapstone/Controllers/AdminController.cs
+++ LaunchCodeCapstone/LaunchCodeCapstone/Controllers/AdminController.cs
@@ -1,22 +1,26 @@
 ﻿using LaunchCodeCapstone.Models.ViewModels;
 using LaunchCodeCapstone.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SendGrid.Helpers.Mail;
 
 namespace LaunchCodeCapstone.Controllers
 {
-    //[Authorize(Roles ="Admin")]
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
 
         private readonly IUserRepository userRepository;
-
-        public AdminController(IUserRepository userRepository)
+        private readonly UserManager<IdentityUser> userManager;
+        public AdminController(IUserRepository userRepository,
+            UserManager<IdentityUser> userManager)
         {
             this.userRepository = userRepository;
+            this.userManager = userManager;
         }
 
+        [HttpGet]
          public async Task<IActionResult> List()
         {
             //get a list of all user in the database from the repo
@@ -39,5 +43,20 @@ namespace LaunchCodeCapstone.Controllers
 
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            
+            if(user != null)
+            {
+                var result = await userManager.DeleteAsync(user);
+                if(result != null && result.Succeeded)
+                {
+                    return RedirectToAction("List", "Admin");
+                }
+            }
+            return View();
+        }
     }
 }
